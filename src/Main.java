f9d7316b005bfc02c028fc5945f5f899eb2e8065
Anna M <ankaanna@gmail.com>
2023-08-26 18:37:50 +0200

    poprawka

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World 123456789");
+        System.out.println("Hello World 1234567890");
         System.out.println("Hello World 1289");
+
     }
 }
